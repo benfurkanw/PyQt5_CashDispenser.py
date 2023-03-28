95ceffc3f38e810d2c76000dd86b8fea0c15d72e
Furkan Demir <furkandemir.tr41@gmail.com>
2023-03-28 18:17:33 +0300

    Login Fonksiyonu Eklendi Register Düzenlendi

--- main.py
+++ main.py
@@ -10,7 +10,21 @@ class CashDispenser:
         time.sleep(1)
 
     def Login(self):
-        pass
+        # user = input("Please enter your user id: ")
+        id = input("ID'nizi girin: ")
+        password = input("Şifrenizi girin: ")
+        with open('registered_people.json', 'r', encoding='utf-8') as f:
+            data = json.load(f)
+
+        for user in data:
+            if str(user["id"]) == id and user["password"] == password:
+                print(f"Giriş başarılı. Hoş geldiniz {user['name']}!")
+                return
+            
+
+        print("Giriş başarısız. Lütfen ID ve şifrenizi kontrol edin.")
+        self.Login()
+            
 
     def Register(self):
         # Prompt the user to enter their information
@@ -50,20 +64,28 @@ class CashDispenser:
             # Prompt the user to select an action
             self.answer = input("Please choose an action:\nLogin -> 1\nRegister -> 2\nExit -> 3\nYour choice: ")
             if self.answer == "1":
-                while True:
                     self.Login()
             
             elif self.answer == "2":
                 while True:
                     self.Register()
-
                     # If the user presses "0", go back
                     if self.Completed == "0":
-                        print("Your information was not saved.")
+                        with open('registered_people.json', 'r+', encoding='utf-8') as f:
+                            # If the file is empty, create a new list
+                            data = f.read()
+                            if len(data) == 0:
+                                registered_people_list = []
+                            else:
+                                # Convert the JSON string to a Python list
+                                registered_people_list = json.loads(data)
+                            print("your user id: " , len(registered_people_list) + 3150)
+                        print("Your information was saved. Please login")
+                        self.Login()
                         break
                     # Otherwise, confirm that the information was saved
                     else:
-                        print("Your information was saved.")
+                        print("Your information was not saved.")
             elif self.answer == "3":
                 break
             
